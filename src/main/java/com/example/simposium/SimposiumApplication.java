336dd4efe28e2a096beec3f469ef27579f02bd06
Dave-GV <josedavigarciaverdugo@gmail.comm>
2026-03-18 14:34:50 -0600

    build:fist commit

---
+++ src/main/java/com/example/simposium/SimposiumApplication.java
@@ -0,0 +1,13 @@
+package com.example.simposium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimposiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimposiumApplication.class, args);
+	}
+
+}
